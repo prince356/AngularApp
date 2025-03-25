e1c4d036b333e69d0f215876a95c8bf4e4014b7d
prince356 <princechaurasia014@gmail.com>
2025-03-25 10:56:17 +0530

    Added the Image Redirect

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,9 +9,14 @@ import { Component, NgModule } from '@angular/core';
 export class AppComponent {
   title = 'HelloWorld';
   imgURL = "https://www.bridgelabz.com/assets/images/BridgeLabz%20New%20Logo.svg";
+  url = "https://www.bridgelabz.com";
 
 
   ngOnInit() : void {
     this.title = "Hello From BridgeLabz."
+  }
+  onClick($event: any) {
+    console.log("Image is Clicked!" , $event);
+    window.open(this.url , "_blank");
   }
 }
